698676370a352f91827c638299b19e80029de95d
belfager77 <si_rutter@hotmail.com>
2025-12-28 11:49:20 +0000

    Ch4L1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
 
@@ -13,8 +15,12 @@ def main():
     dt=0
     updatable=pygame.sprite.Group()
     drawable=pygame.sprite.Group()
+    asteroids=pygame.sprite.Group()
     Player.containers=(updatable,drawable)
+    Asteroid.containers=(updatable,drawable,asteroids)
+    AsteroidField.containers=updatable
     p=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    af=AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
